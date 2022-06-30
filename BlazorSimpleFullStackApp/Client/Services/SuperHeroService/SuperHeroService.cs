de2bdff9456e9cff5439f7f94053da267ae13665
LukSt92 <stach.stachu@gmail.com>
2022-06-30 23:23:29 +0200

    implemented editform.

--- BlazorSimpleFullStackApp/Client/Services/SuperHeroService/SuperHeroService.cs
+++ BlazorSimpleFullStackApp/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -14,9 +14,13 @@ namespace BlazorSimpleFullStackApp.Client.Services.SuperHeroService
         public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
         public List<Comic> Comics { get; set; } = new List<Comic> { };
 
-        public Task GetComics()
+        public async Task GetComics()
         {
-            throw new NotImplementedException();
+            var result = await _http.GetFromJsonAsync<List<Comic>>("api/superhero/comics");
+            if (result != null)
+            {
+                Comics = result;
+            }
         }
 
         public async Task<SuperHero> GetSingleHero(int id)
