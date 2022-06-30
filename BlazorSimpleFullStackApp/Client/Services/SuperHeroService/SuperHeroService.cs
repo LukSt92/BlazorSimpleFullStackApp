f29da82f048a87f15f5e6a5750fabaa5d53edd16
LukSt92 <stach.stachu@gmail.com>
2022-06-30 21:46:59 +0200

    Added SuperHeroes pages with logic, Hero page and Services.

---
+++ BlazorSimpleFullStackApp/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorSimpleFullStackApp.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _http;
+
+        public SuperHeroService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic> { };
+
+        public Task GetComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<SuperHero> GetSingleHero(int id)
+        {
+            var result = await _http.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+            if (result != null)
+            {
+                return result;
+            }
+            throw new Exception("Hero not found!");
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+            if (result !=null)
+            {
+                Heroes = result;
+            }
+        }
+    }
+}
